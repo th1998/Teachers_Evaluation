e99d2c4264237cfdbb5a7e7d590bb358516d2dcd
tanghao <850528794@qq.com>
2019-09-05 09:10:24 +0800

    test1

---
+++ src/main/java/com/teachers/TeachersApplication.java
@@ -0,0 +1,13 @@
+package com.teachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachersApplication.class, args);
+    }
+
+}
